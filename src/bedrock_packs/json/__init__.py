36b88e2b68769c0940ff09b11ee48a1750bd81c2
Nusiq <nusiq@outlook.com>
2021-01-13 21:03:32 +0100

    Reorganized project structure to hide some of the imports. Renamed some classes.

---
+++ src/bedrock_packs/json/__init__.py
@@ -0,0 +1,8 @@
+from ._content import (
+    JSONCDecoder,
+    CompactEncoder,
+
+    JSONWalker, JSONWalkerDict, JSONWalkerList, JSONWalkerStr, JSONWalkerFloat,
+    JSONWalkerInt, JSONWalkerBool, JSONWalkerNone, JSONWalkerInvalidPath,
+    JSONWalkerIndexError, JSONWalkerKeyError, JsonSplitWalker,
+)
